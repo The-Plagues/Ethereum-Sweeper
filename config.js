5b2b0915139ed63701ece83b566091ac926e8e9f
The-Plagues <124101696+The-Plagues@users.noreply.github.com>
2023-04-17 15:48:18 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  privateKey: "Private Key Of Scam Wallet Here",
+  targetAddress: "Address to where the ETH will go",
+  providerUrl: 'https://mainnet.infura.io/v3/Replace it with your Infura API KEY',
+};
